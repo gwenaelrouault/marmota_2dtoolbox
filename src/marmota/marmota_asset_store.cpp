52eba8a35f81b261ae413dbe460e9e3db799f41b
gwenaelrouault <gwenael.rouault@gmail.com>
2025-11-23 01:00:44 +0100

    enable sqlite foreign key

--- src/marmota/marmota_asset_store.cpp
+++ src/marmota/marmota_asset_store.cpp
@@ -10,5 +10,14 @@ void MarmotaAssetStore::open(const filesystem::path& path) {
         sqlite3_close(index_db);
         throw DBException("Cannot open new project : cannot open metadata database");
     }
+    char* err = nullptr;
+    rc = sqlite3_exec(index_db, "PRAGMA foreign_keys = ON;", nullptr, nullptr, &err);
+    if (rc) {
+        sqlite3_close(index_db);
+        throw DBException("Cannot open new project : cannot open metadata database");
+    }
     _db_index = make_index_db(index_db);
+    _table_entity->create();
+    _table_state->create();
+    _table_entity->create();
 }
